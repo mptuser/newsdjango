227abf1002ec64f6369dddf0727044d2bd6cd351
mptuser <i.a.morozov@mpt.ru>
2020-05-17 02:45:29 +0300

    fix heroku

--- NewsDjango/settings.py
+++ NewsDjango/settings.py
@@ -114,10 +114,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
